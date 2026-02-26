d962ec6e6736f1a81e18a0db75f86067d18ae76a
LeafS825 <871850079@qq.com>
2026-02-26 19:25:48 +0800

    fix(android): 修正包名中的连字符为下划线

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 }
 
 android {
-    namespace = "com.leafs825.secrandom-lutter"
+    namespace = "com.leafs825.secrandom_lutter"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -29,7 +29,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.leafs825.secrandom-lutter"
+        applicationId = "com.leafs825.secrandom_lutter"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
